790d187e0583225844c3c58803f9a117304bf93d
Alexandr Tyulenev <alcrysdd@gmail.com>
2021-09-16 14:52:22 +0600

    add api_token fielad to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->rememberToken()->nullable();
+            $table->string('api_token')->nullable();
             $table->timestamps();
         });
     }
