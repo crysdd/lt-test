714bef70ca2fd3a0ed77d592c0adb30f469e91e0
alex <alcrysdd@gmail.com>
2021-09-19 16:45:31 +0600

    tasks negative tests

--- tests/Feature/V1/TasksTest.php
+++ tests/Feature/V1/TasksTest.php
@@ -30,23 +30,30 @@ class TasksTest extends TestCase
         $response = $this->get('/api/v1/tasks');
 
         $response->assertStatus(Response::HTTP_OK)
-        ->assertJsonStructure(
-            [
-                'current_page',
-                'data' => [
-                    '*' => [
-                        'id',
-                        'user_id',
-                        'day',
-                        'done',
-                        'category_id',
-                        'name',
-                        'text',
-                        'category_name'
+            ->assertJsonStructure(
+                [
+                    'current_page',
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'user_id',
+                            'day',
+                            'done',
+                            'category_id',
+                            'name',
+                            'text',
+                            'category_name'
+                        ]
                     ]
                 ]
-            ]
-        );
+            );
+    }
+
+    public function testUnauthorizedTasks()
+    {
+        $response = $this->get('/api/v1/tasks');
+
+        $response->assertStatus(Response::HTTP_FOUND);
     }
 
     public function testReReleaseTask()
@@ -69,25 +76,68 @@ class TasksTest extends TestCase
             'id' => $task->id,
         ];
         $this->assertDatabaseHas('tasks', ['id' => $task->id]);
+        if ($this->assertDatabaseCount('tasks', 1)) {
+            $this->json('put', 'api/v1/tasks/change-task', $payload)
+                ->assertStatus(Response::HTTP_NOT_FOUND)
+                ->assertJson(
+                    [
+                        'errors' => [
+                            'No one new exercise exists'
+                        ]
+                    ]
+                );
+        }else{
+            $this->json('put', 'api/v1/tasks/change-task', $payload)
+                ->assertStatus(Response::HTTP_OK)
+                ->assertJsonStructure(
+                    [
+                        'data' => [
+                            'id',
+                            'user_id',
+                            'day',
+                            'done',
+                            'category_id',
+                            'name',
+                            'text',
+                            'category_name'
+                        ]
+                    ]
+                );
 
-        $this->json('put', 'api/v1/tasks/change-task', $payload)
-        ->assertStatus(Response::HTTP_OK)
-        ->assertJsonStructure(
-            [
-                'data' => [
-                    'id',
-                    'user_id',
-                    'day',
-                    'done',
-                    'category_id',
-                    'name',
-                    'text',
-                    'category_name'
-                ]
-            ]
+            $this->assertDatabaseMissing('tasks', ['id' => $task->id]);
+        }
+    }
+
+    public function testWrongUserReReleaseTasks()
+    {
+        $user = Sanctum::actingAs(
+            User::factory()->create(),
+            ['tasks']
         );
-
-        $this->assertDatabaseMissing('tasks', ['id' => $task->id]);
+        $exercise = Exercise::factory()->create([
+            'category_id' => 1,
+            'name' => 'Exercise Name',
+            'text' => 'Exercise Text',
+        ]);
+        $task = Task::factory()->create([
+            'user_id' => $user->id,
+            'exercise_id' => $exercise->id,
+            'day' => now()->format('Y-m-d'),
+        ]);
+        $payload = [
+            'id' => $task->id - 1,
+        ];
+        $this->json('put', 'api/v1/tasks/change-task', $payload)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson(
+                [
+                    "errors" => [
+                        "id" => [
+                            "The selected id is invalid."
+                        ]
+                    ]
+                ]
+            );
     }
 
     public function testDone()
@@ -111,17 +161,50 @@ class TasksTest extends TestCase
         ];
 
         $this->json('put', 'api/v1/tasks/done', $payload)
-        ->assertStatus(Response::HTTP_OK)
-        ->assertJson(
-            [
-                'data' => [
-                    'success' => 'true',
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJson(
+                [
+                    'data' => [
+                        'success' => 'true',
+                    ]
                 ]
-            ]
-        );
+            );
 
         $changed_task = Task::find($task->id);
         $this->assertEquals($changed_task->done, true);
 
+    }
+
+    public function testWrongUserDone()
+    {
+        $user = Sanctum::actingAs(
+            User::factory()->create(),
+            ['tasks']
+        );
+        $exercise = Exercise::factory()->create([
+            'category_id' => 1,
+            'name' => 'Exercise Name',
+            'text' => 'Exercise Text',
+        ]);
+        $task = Task::factory()->create([
+            'user_id' => $user->id,
+            'exercise_id' => $exercise->id,
+            'day' => now()->format('Y-m-d'),
+        ]);
+        $payload = [
+            'id' => $task->id - 1,
+        ];
+
+        $this->json('put', 'api/v1/tasks/done', $payload)
+            ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson(
+                [
+                    "errors" => [
+                        "id" => [
+                            "The selected id is invalid."
+                        ]
+                    ]
+                ]
+            );
     }
 }
